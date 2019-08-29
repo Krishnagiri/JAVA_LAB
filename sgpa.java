b9704f89510849b2fcccc6b3fbff96da9d2a6112
Krishnagiri <54149166+Krishnagiri@users.noreply.github.com>
2019-08-29 15:53:23 +0530

    Add files via upload

--- sgpa.java
+++ sgpa.java
@@ -1,88 +1,68 @@
 import java.util.*;
+class stud
+{
+	String name=new String(); 
+	String usn=new String();
+	int marks[] = new int[5];
+	int cred[] = new int[5];
+	int grad[] = new int[5];
+	double sgpa;
+	
+	void get_data()
+	{Scanner s = new Scanner(System.in);
+	 System.out.println("Enter the name of student");
+	name = s.next();
+	System.out.println("Enter the usn of student");
+	usn = s.next();
+	System.out.println("Enter the marks in 5 subjects");
+	for(int i=0;i<5;i++)	
+	{marks[i] = s.nextInt();}
+	System.out.println("Enter the credits ");
+	for(int i=0;i<5;i++)	
+	{cred[i] = s.nextInt();}
+	}
+		
+	void calc()
+	{
+	 for(int i=0;i<5;i++)
+	 {if(marks[i]>=90)
+		grad[i]=10;
+	 else if(marks[i]<90&&marks[i]>=75)
+	  	grad[i]=9;
+	else if(marks[i]<75&&marks[i]>=60)
+	        grad[i]=8;
+	else if(marks[i]<60&&marks[i]>=50)
+		grad[i]=7;
+	else if(marks[i]<50&&marks[i]>=45)
+		grad[i]=5;
+	else if(marks[i]<45&&marks[i]>=40)
+		grad[i]=4;
+	else if(marks[i]<40)
+		grad[i]=0;
+	}
+	}
+ 	void disp()
+	{
+	System.out.println("Name of student :" + name);
+	System.out.println("USN of Student  :" + usn);
+	System.out.println("SGPA of student :" +sgpa);
+	}
+	void calc_sgpa()
+	{int tot_crd=0;int tot=0;
+	 for(int i=0;i<5;i++)
+	{tot_crd+=cred[i];}
+	 for(int i=0;i<5;i++)
+	{tot+=grad[i]*cred[i];}
+	sgpa=tot/tot_crd;
+	}
+}
 class sgpa
-   {
-     int n;
-     int marks[];
-     int credits[];
-     int grades[];
-     float total_credits=0;
-    
-     float total=0;
-     
-     sgpa(int n)
-     {this.n=m
-      this.marks=new int[n];;
-      this.credits=new int[n];
-      this.grades=new int[n];
-       
-       
-       }
-     
-     void calc1()
-     {
-      Scanner sc=new Scanner(System.in);
-      for(int i=0;i<n;i++)
-      {
-        System.out.println("Enter sub marks");
-        marks[i]=sc.nextInt();
-        System.out.println("Enter sub creds");
-        credits[i]=sc.nextInt();
-        total_credits+=credits[i];
-       }
-       
-       
-     void calcf()
-     {
-       for(int i=0;i<n;i++)
-       {
-          if(marks[i]<=90)
-            {
-             grades[i]=10;
-             total+=grades[i]*credits[i];
-             }
-           else  if(marks[i]<=75)
-            {
-             grades[i]=9;
-             total+=grades[i]*credits[i];
-             }
-            else if(marks[i]<=60)
-            {
-             grades[i]=8;
-             total+=grades[i]*credits[i];
-             }
-            else if(marks[i]<=50)
-            {
-             grades[i]=7;
-             total+=grades[i]*credits[i];
-             }
-            else if(marks[i]<=40)
-            {
-             grades[i]=6;
-             total+=grades[i]*credits[i];
-             }
-             else 
-            {
-             grades[i]=0;
-             total+=0;
-             }
-             
-             }
-             
-             }
-              void display()
-              {
-               System.out.println(" sgpa= " +total/total_credits);
-              } }
-               
-        class sgpamain
-        {
-         public static void main(String args[])
-         {Scanner sc=new Scanner(System.in);
-          System.out.println("Enter no of subjects");
-          int n=sc.nextInt();
-          sgpa s1=new sgpa(n);
-          s1.calc1();
-          s1.calcf();
-          s1.display();}
-          }       
-       
+{
+	public static void main(String x[])
+	{stud s1=new stud();
+	s1.get_data();
+	s1.calc();
+	s1.calc_sgpa();
+	s1.disp();
+	}
+}
